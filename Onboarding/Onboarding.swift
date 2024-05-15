dfbc4e8a9545714e93db26319c371767fa8cbf20
Sachin Agrawal <95291400+SachinSAgrawal@users.noreply.github.com>
2024-05-15 16:01:11 -0400

    Add files via upload

---
+++ Onboarding/Onboarding.swift
@@ -0,0 +1,256 @@
+//
+//  Onboarding.swift
+//  Onboarding
+//
+//  Created by Sachin Agrawal on 3/8/24.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController, UIScrollViewDelegate {
+
+    // MARK: Onboarding Data
+    let onboardingData = [
+        OnboardingData(
+            icon: "gamecontroller",
+            header: "Gaming",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        ),
+        OnboardingData(
+            icon: "airplane",
+            header: "Flights",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        ),
+        OnboardingData(
+            icon: "map.circle",
+            header: "Information",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        ),
+        OnboardingData(
+            icon: "exclamationmark.shield",
+            header: "Warning",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        ),
+        OnboardingData(
+            icon: "bell",
+            header: "Notifications",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        ),
+        OnboardingData(
+            icon: "bookmark",
+            header: "Bookmarks",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        )
+    ]
+    
+    // Page control for indicating current page
+    let pageControl: UIPageControl = {
+        let pageControl = UIPageControl()
+        pageControl.currentPageIndicatorTintColor = UIColor.systemGray
+        pageControl.pageIndicatorTintColor = UIColor.systemGray4
+        return pageControl
+    }()
+
+    // Scroll view for displaying onboarding screens
+    lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.isPagingEnabled = true
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.delegate = self
+        return scrollView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+    }
+
+    // MARK: Setup Methods
+    func setupViews() {
+        view.addSubview(scrollView)
+        scrollView.frame = view.bounds
+        
+        // Add swipe gestures for scrolling through pages
+        let leftSwipeGesture = UISwipeGestureRecognizer(target: self, action: #selector(handleLeftSwipe(_:)))
+        leftSwipeGesture.direction = .left
+        scrollView.addGestureRecognizer(leftSwipeGesture)
+
+        let rightSwipeGesture = UISwipeGestureRecognizer(target: self, action: #selector(handleRightSwipe(_:)))
+        rightSwipeGesture.direction = .right
+        scrollView.addGestureRecognizer(rightSwipeGesture)
+        
+        // Add page control
+        pageControl.addTarget(self, action: #selector(pageControlValueChanged), for: .valueChanged)
+        
+        // Add onboarding views
+        var previousView: UIView?
+        for (_, data) in onboardingData.enumerated() {
+            let onboardingView = OnboardingView(data: data)
+            scrollView.addSubview(onboardingView)
+            onboardingView.translatesAutoresizingMaskIntoConstraints = false
+
+            // Set constraints for onboarding view
+            NSLayoutConstraint.activate([
+                onboardingView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+                onboardingView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+                onboardingView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+                onboardingView.heightAnchor.constraint(equalTo: scrollView.heightAnchor),
+                onboardingView.leadingAnchor.constraint(equalTo: previousView?.trailingAnchor ?? scrollView.leadingAnchor)
+            ])
+
+            previousView = onboardingView
+        }
+        
+        // Set scroll view content size and page control properties
+        scrollView.contentSize = CGSize(width: scrollView.frame.width * CGFloat(onboardingData.count), height: scrollView.frame.height)
+        scrollView.isPagingEnabled = true
+
+        pageControl.numberOfPages = onboardingData.count
+        pageControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(pageControl)
+
+        NSLayoutConstraint.activate([
+            pageControl.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            pageControl.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50)
+        ])
+        
+        // Add continue button
+        let continueButton = UIButton()
+        continueButton.setTitle("Continue", for: .normal)
+        continueButton.setTitleColor(.systemBlue, for: .normal)
+        continueButton.addTarget(self, action: #selector(continueButtonTapped), for: .touchUpInside)
+        continueButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(continueButton)
+        
+        // Set constraints for continue button
+        NSLayoutConstraint.activate([
+            continueButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            continueButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100),
+            continueButton.widthAnchor.constraint(equalToConstant: 150),
+            continueButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+
+    // MARK: Action Methods
+    // Handle left swipe gesture
+    @objc func handleLeftSwipe(_ gesture: UISwipeGestureRecognizer) {
+        handleSwipe(gesture, direction: .left)
+        print("left")
+    }
+
+    // Handle right swipe gesture
+    @objc func handleRightSwipe(_ gesture: UISwipeGestureRecognizer) {
+        handleSwipe(gesture, direction: .right)
+        print("right")
+    }
+
+    // Handle swipe gesture
+    @objc func handleSwipe(_ gesture: UISwipeGestureRecognizer, direction: UISwipeGestureRecognizer.Direction) {
+        // Determine next page based on swipe direction
+        let currentPage = pageControl.currentPage
+        var nextPage: Int
+        
+        print(direction)
+        switch direction {
+        case .left:
+            nextPage = min(currentPage + 1, onboardingData.count - 1)
+        case .right:
+            nextPage = max(currentPage - 1, 0)
+        default:
+            return
+        }
+
+        // Set content offset of scroll view to scroll to next page
+        let xOffset = scrollView.frame.width * CGFloat(nextPage)
+        scrollView.setContentOffset(CGPoint(x: xOffset, y: 0), animated: true)
+        pageControl.currentPage = nextPage
+    }
+
+    // Handle continue button tap
+    @objc func continueButtonTapped() {
+        let currentPage = pageControl.currentPage
+
+        // If not on last page, scroll to next page, and otherwise, perform segue to main view
+        if currentPage != onboardingData.count - 1 {
+            let nextPage = min(currentPage + 1, onboardingData.count - 1)
+            let xOffset = scrollView.frame.width * CGFloat(nextPage)
+            scrollView.setContentOffset(CGPoint(x: xOffset, y: 0), animated: true)
+            pageControl.currentPage = nextPage
+        } else {
+            performSegue(withIdentifier: "toMainView", sender: self)
+        }
+    }
+    
+    // Handle page control value change
+    @objc func pageControlValueChanged() {
+        // Scroll to the selected page
+        let newPage = pageControl.currentPage
+        let xOffset = scrollView.frame.width * CGFloat(newPage)
+        scrollView.setContentOffset(CGPoint(x: xOffset, y: 0), animated: true)
+    }
+}
+
+// Struct to hold data for each onboarding screen
+struct OnboardingData {
+    let icon: String
+    let header: String
+    let description: String
+}
+
+// MARK: Onboarding View
+// Custom view for displaying onboarding content
+class OnboardingView: UIView {
+
+    // Initialize view with onboarding data
+    init(data: OnboardingData) {
+        super.init(frame: .zero)
+
+        // Add icon image view
+        let iconImageView = UIImageView(image: UIImage(systemName: data.icon))
+        iconImageView.tintColor = .systemBlue
+        iconImageView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(iconImageView)
+
+        // Add header label
+        let headerLabel = UILabel()
+        headerLabel.text = data.header
+        headerLabel.font = UIFont.systemFont(ofSize: 32)
+        headerLabel.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(headerLabel)
+
+        // Add description label
+        let descriptionLabel = UILabel()
+        descriptionLabel.text = data.description
+        descriptionLabel.font = UIFont.systemFont(ofSize: 24)
+        descriptionLabel.numberOfLines = 0
+        descriptionLabel.textAlignment = .center
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        descriptionLabel.textColor = UIColor.secondaryLabel
+        addSubview(descriptionLabel)
+        
+        // Set constraints for views
+        if let image = iconImageView.image {
+            let aspectRatio = image.size.width / image.size.height
+            let widthConstraint = iconImageView.widthAnchor.constraint(equalToConstant: 110)
+            let heightConstraint = iconImageView.heightAnchor.constraint(equalTo: iconImageView.widthAnchor, multiplier: 1.0 / aspectRatio)
+
+            NSLayoutConstraint.activate([
+                widthConstraint,
+                heightConstraint,
+                iconImageView.centerXAnchor.constraint(equalTo: centerXAnchor),
+                iconImageView.centerYAnchor.constraint(equalTo: centerYAnchor, constant: -100),
+
+                headerLabel.topAnchor.constraint(equalTo: iconImageView.bottomAnchor, constant: 20),
+                headerLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+
+                descriptionLabel.topAnchor.constraint(equalTo: headerLabel.bottomAnchor, constant: 20),
+                descriptionLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+                descriptionLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20)
+            ])
+        }
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ README.md
@@ -0,0 +1,42 @@
+# Onboarding
+
+![demo](demo.gif)
+
+## About
+I needed to add some simple onboarding cards to another app I have been working on, and I could not find anything that was quite what I wanted, so I created this app. If you like it or found it useful, I would appreciate if you starred it or even shared it with your friends. I don't expect to work on this too much more, as I am quite satisfied with the end result.
+
+
+## Acknowledgments
+My app is roughly based on this [tutorial](https://www.youtube.com/watch?v=cpg7f4pVzFw) by iOS Academy that uses SwiftUI for a simple onboarding app. However, I needed to use UIKit, so I basically converted their app from SwiftUI, and added some of my own features.
+
+## Usage
+Everytime the app is launched, the onboarding cards should appear, with an icon, header, and text. Navigate through them by pressing `Continue`, swiping left and right, or scrolling through the pagination dots. Once you have reached the last page, hitting `Continue` takes you to what would theoretically be the main section of your app. Here, it is simply a button that allows you to start over. 
+
+## Implementation
+To add this into your own app, simply copy the code from the`.Swift` files into your existing project. In your own `Main.Storyboard`, create a new view controller, and set its class to be `OnboardingViewController`. Then change the data inside `Onboarding.Swift`, where icon can be any SF Symbol, to fit your app. Right now, the data is just some placeholder lipsum text. You can change the number of pages by simply adding or deleting `OnboardingData` structures. Lastly add a segue to your main view controller from the onboarding view controller with the identifier `toMainView`.
+
+## Installation
+1. Clone this repository or download it as a zip folder and uncompress it.
+2. Open up the `.xcodeproj` file, which should automatically launch Xcode.
+3. You might need to change the signing of the app from the current one.
+4. Click the `Run` button near the top left of Xcode to build and install.
+
+#### Prerequisites
+Hopefully this goes without saying, but you need Xcode, which is only available on Macs.
+
+#### Notes
+You can run this app on the Xcode simulator or connect a physical device. <br>
+The device must be either an iPhone or iPad running iOS 16.0 or newer.
+
+## SDKs
+* [UIKit](https://developer.apple.com/documentation/uikit/) - Construct and manage a graphical, event-driven user interface for your iOS, iPadOS, or tvOS app.
+* [Swift](https://developer.apple.com/swift/) - A powerful and intuitive programming language for all Apple platforms.
+
+## Bugs
+If you find one, feel free to open up a new issue or even better, create a pull request fixing it. But then again, this app is extremely simple, so I don't expect for there to be any.
+
+## Contributors
+Sachin Agrawal: I'm a self-taught programmer who knows many languages and I'm into app, game, and web development. For more information, check out my website or Github profile. If you would like to contact me, my email is [github@sachin.email](mailto:github@sachin.email).
+
+## License
+This package is licensed under the [MIT License](LICENSE.txt).
